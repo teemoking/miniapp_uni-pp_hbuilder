fb9ac71f8731add61aca5ac129d13b1c165834cc
teemoking <AK1028430241@gmail.com>
2020-10-07 15:42:05 +0800

    spring boot hello world

---
+++ src/main/java/spring/hello/demo/TeeApplication.java
@@ -0,0 +1,19 @@
+package spring.hello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeApplication.class, args);
+	}
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello spring";
+	}
+}
---
+++ src/test/java/spring/hello/demo/TeeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.hello.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
